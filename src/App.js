dad6fa082358654b822274f019c4e0b16005beaa
Prajul <prajulaadityaa@gmail.com>
2023-01-13 03:29:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from "react";
+import {Cryptocurrencies, CryptoDetails, Exchanges, Homepage, Navbar, News} from "./components";
 import './App.css';
+import {Link, Route, Switch} from "react-router-dom";
+import {Layout, Space, Typography} from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <React.Fragment>
+            <div className="app">
+                <div className="navbar">
+                    <Navbar/>
+                </div>
+                <div className="main">
+                    <Layout>
+                        <div className="routes">
+                            <Switch>
+                                <Route exact path="/">
+                                    <Homepage/>
+                                </Route>
+                                <Route exact path="/exchanges">
+                                    <Exchanges/>
+                                </Route>
+                                <Route exact path="/cryptocurrencies">
+                                    <Cryptocurrencies/>
+                                </Route>
+                                <Route exact path="/crypto/:coinID"> {/* meaning of ':' is the value will be dynamic */}
+                                    <CryptoDetails/>
+                                </Route>
+                                <Route exact path="/news">
+                                    <News/>
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Layout>
+                    <div className="footer">
+                        <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                            InfoCrypt, <br/>
+                            All rights reserved.
+                        </Typography.Title>
+                        <Space>
+                            <Link to="/">Home</Link>
+                            <Link to="/exchanges">Exchanges</Link>
+                            <Link to="/news">News</Link>
+                        </Space>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default App;
